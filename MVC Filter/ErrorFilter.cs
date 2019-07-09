5a8e09f2a55facb5b0c49e36128b7de8af804ea6
abcd597 <abcd597@hotmail.com>
2019-07-09 09:57:59 +0800

    Add files via upload

---
+++ MVC Filter/ErrorFilter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MemberApi.Models.Filter
+{
+    public class ErrorFilter : HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            var errorMessage = filterContext.Exception.Message;
+            filterContext.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            filterContext.HttpContext.Response.TrySkipIisCustomErrors = true;
+            var result = new JsonModel()
+            {
+                code = (int)ApiStatus.status.fail,
+                message = errorMessage
+            };
+            filterContext.Result = new JsonResult
+            {
+                Data = result,
+                ContentEncoding = System.Text.Encoding.UTF8,
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
+            filterContext.ExceptionHandled = true;
+        }
+    }
+}
---
+++ MVC Filter/ParameterFilter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MemberApi.Models.Filter
+{
+    public class ParameterFilter : ActionFilterAttribute
+    {
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            if (filterContext.Result is JsonResult)
+            {
+                var result = filterContext.Result as JsonResult;
+                var data = result.Data;
+                JsonModel returnData = new JsonModel();
+                returnData.code = (int)ApiStatus.status.success;
+                returnData.message = "成功";
+                returnData.data = data;
+                result.Data = returnData;
+            }
+
+        }
+    }
+}
